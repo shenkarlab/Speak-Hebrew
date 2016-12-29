90ab6f2b1d7f21795842d22d428d6ab2b20deb9a
Daria Dadov <daria.dadov@imperva.com>
2016-12-29 18:10:39 +0200

    Get URL and parsing DOM. Takes all heb word and push them into array

--- server/controllers/textualLogicController.js
+++ server/controllers/textualLogicController.js
@@ -1,6 +1,34 @@
-exports.getTranslatableWords = function(documentObjectModel,dictionary){
-    console.log("dom:");
-    console.log(documentObjectModel);
+exports.getTranslatableWords = function(url,dictionary){
+    var jsdom = require("jsdom");
+
+    jsdom.env(
+        url,
+        ["http://code.jquery.com/jquery.js"],
+        function (err, window) {
+            var str = (window.$("body").text());
+            str = JSON.stringify(str);
+            var result = "";
+            for(var i = 0; i < str.length;i++){
+                if (str.charCodeAt(i) >= 0x590 && str.charCodeAt(i) <= 0x5FF){
+                    result+=str[i];
+                }
+                else{
+                    result+=" ";
+                }
+            }
+            var resultArray = result.split(" ");
+            var hebrewWords = [];
+            resultArray.forEach(function (str) {
+                if(str.length > 0){
+                    hebrewWords.push(str);
+                }
+            });
+            console.log(hebrewWords);
+            console.log("-----------------------------------");
+        });
+
+
+
     var resultData = [];
     //temporary hardcodet result for testing
     resultData.push({
