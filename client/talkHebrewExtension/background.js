521c76e0a271252515b646b0167e51d3b9fa5316
or amit <or amit>
2017-01-03 18:44:09 +0200

    extension PopUp

--- client/talkHebrewExtension/background.js
+++ client/talkHebrewExtension/background.js
@@ -1,8 +1,3 @@
-console.log("back.js file EXE!");
-
-var pageUrl=window.location.href;
-console.log('url is'+pageUrl);
-
 //static local words for testing
 // var wordsToreplace1=[
 //     {   "latin":"ג'קוזי",
@@ -20,17 +15,73 @@ console.log('url is'+pageUrl);
 // ];
 
 
+console.log("back.js file EXE!");
+var enableAutoReplaceWordsBool;
+var pageUrl=window.location.href;
+console.log('url is'+pageUrl);
 
-var wordsToreplace2;
+
+
+
+
+function updateNumberOfWordsThatAutoReplace(numOfWords){
+    var totalUserAmounth;
+    console.log("the number of words in the page to save is: "+numOfWords);
+    chrome.storage.local.set({ "numOfAutoReplaceWordsInPage": numOfWords }, function(){
+        console.log("saved in storage. number of words is:"+numOfWords);
+
+    });
+
+    chrome.storage.local.get(["totalNumberOfUserWords"], function(items){
+        totalUserAmounth=items.totalNumberOfUserWords;
+        if(totalUserAmounth==undefined){
+            console.log("total isnt deined");
+            totalUserAmounth=numOfWords;
+            chrome.storage.local.set({ "totalNumberOfUserWords": totalUserAmounth }, function(){
+                console.log("total amounth saved: "+totalUserAmounth);
+
+            });
+
+        }
+        else{
+            console.log("total is defined");
+            totalUserAmounth+=numOfWords;
+            chrome.storage.local.set({ "totalNumberOfUserWords": totalUserAmounth }, function(){
+                console.log("total amounth is is:"+totalUserAmounth);
+
+            });
+
+        }
+        //document.getElementById("numberOfWordsInPage").innerHTML=numOfWords+" ";
+
+        // talkUser=items.talkHebrewUser;
+        // if(talkUser==undefined){
+        //     currenntUserSpan_element.innerHTML="לא מוגדר משתמש";
+        //
+        // }
+        // else {
+        //     currenntUserSpan_element.innerHTML="שלום: "+talkUser;
+        // }
+    });
+
+}
 
 function setClickebleFuncionToAllElements(){
-    var latinWordsArray = document.getElementsByClassName('press');
+    console.log("back.js: setClickebleFuncionToAllElements");
+    var latinWordsArray = document.getElementsByClassName('latinWords');
+    //var myPoUpArr=document.getElementsByClassName('popuptext');
     // onClick's logic below:
-    console.log("number of words that was swiched" +latinWordsArray.length);
+    console.log("number of words that was swicheddddd:" +latinWordsArray.length);
+    //console.log("number of popUps:" +myPoUpArr.length);
+
     var latinWordsAmounth=latinWordsArray.length;
-    for(var i=0;i<latinWordsAmounth;i++){
-        latinWordsArray[i].addEventListener('click', function () {
-            console.log('clickedMe');
+    updateNumberOfWordsThatAutoReplace(latinWordsAmounth/2);
+
+    var i;
+    for(i=0;i<latinWordsAmounth;i++){
+        latinWordsArray[i].addEventListener('click', function(){
+            //console.log("num of children "+this.childNodes.length);
+            this.childNodes[1].classList.toggle('show');
         });
     }
 }
@@ -39,90 +90,86 @@ function setClickebleFuncionToAllElements(){
 
 var user;
 function  switchWords(){
-    console.log("switchWords(). the user is"+user);
-    var myHtml=document.body.innerHTML;
-    var docBudy=document.body;
-    var myHtml2=docBudy.textContent || docBudy.innerText;
+    console.log("back.js: switchWords(). the user is"+user);
+    var myHtml=document.body.innerHTML; //the current html
 
+    var wordToSearch;   //the latin word in the page.
+    var wordToSearchReg;//the latin word in the page in regular expression.
+    var hebrewWord;     //the hebrew translation of the latin word.
+    var explanation;    //the explanation of the word..
 
-    var wordToSearch;
-    var wordToSearchReg;
-    var newWord;
+    //server response variables
+    var wordsToreplace2; //list of words that comes from the server in order to switch them in the page.
+    var serverResponse;
 
-
-
-            var serverResponse;
-            var myXMLhttpReq=new XMLHttpRequest(),
-            method = "GET",
-            url="https://speak-hebrew-lab-project.herokuapp.com/getUrlHebrewWords/oramit88@gmail.com?url="+pageUrl;
-                //url="localhost:3000/getUrlHebrewWords/pageUrl/oramit88@gmail.com?documentObjectModel="+myHtml;
-
-
-                //to test the console.log with or amit server, change url to:
+    var myXMLhttpReq=new XMLHttpRequest(),
+    method = "GET",
+    url="https://speak-hebrew-lab-project.herokuapp.com/getUrlHebrewWords?url="+pageUrl;
+            //url="localhost:3000/getUrlHebrewWords/pageUrl/oramit88@gmail.com?documentObjectModel="+myHtml;
+            //to test the console.log with or amit server, change url to:
             //url="https://circlews.herokuapp.com/getAllCategories";
-
-            //dima
+            console.log("testttttt:");
+            console.log(url);
             myXMLhttpReq.open(method, url, true);
             myXMLhttpReq.onreadystatechange = function() {
-                console.log("on ready state change");
                 if (myXMLhttpReq.readyState == XMLHttpRequest.DONE) {
                     console.log("Response is: ");
+                    console.log(serverResponse);
                     serverResponse=JSON.parse(myXMLhttpReq.responseText);
                     if(serverResponse.result==="ok"){
-                        console.log(serverResponse.data);
-                        wordsToreplace2=serverResponse.data;
+                        //console.log(serverResponse.data);
+                         wordsToreplace2=serverResponse.data;
 
-                        //words from Dima from server
+                         var numOfWordsToSwich=wordsToreplace2.length;
+                         for(var i=0;i<numOfWordsToSwich;i++){
 
-                         for(var i=0;i<wordsToreplace2.length;i++){
-                             //console.log(i);
-                             //console.log(wordsToreplace2[i].latin);
                              wordToSearch=wordsToreplace2[i].word; //the latin word
-                             console.log("the latin word: "+wordToSearch);
+                             //console.log("the latin word: "+wordToSearch);
+
+                             //create regular expression
                              wordToSearchReg = new RegExp(wordToSearch, "g");
 
-                             newWord=wordsToreplace2[i].translation[0];//the hebrew word
-                             console.log("the hebrew word: "+newWord);
-                             newPage = myHtml.replace(wordToSearchReg, "<span ><b class='press' style='background-color: greenyellow'>"+newWord+"</b></span>");
+                             //get the hebrew word
+                             hebrewWord=wordsToreplace2[i].translation[0];//the hebrew word
+
+                             //get thr explanation
+                             explanation=wordsToreplace2[i].explanation;
+                             if(explanation==null){
+                                 explanation="";
+                             }
+
+                             var newPage; //the new html after the replace
+
+                             //repacing the latin word in hebrew words with diffrent style and set them as clickable elements to show popUp.
+                             newPage = myHtml.replace(wordToSearchReg, "<span class='latinWords'>"
+                                                                                +"<span>"+hebrewWord+"</span>"
+                                                                                +"<span class='popuptext' id='myPopup'>"
+                                                                                        +"<span class='latinWords'>"+hebrewWord+"</span>"
+                                                                                        +"<span class='hebrewWords'>"+wordToSearch+"</span>"
+                                                                                        +"<span class='explenation'><br>"+explanation+"</span>"
+                                                                                +"</span>"
+                                                                        +"</span>");
+                             //update the page with the new page
                              document.body.innerHTML = newPage;
                              myHtml=newPage;
-                             setClickebleFuncionToAllElements();
+
                          }
-
-
-
-                        //words from ors static dictionary
-                        /*
-                        for(var i=0;i<wordsToreplace1.length;i++){
-                            //console.log(i);
-
-                            wordToSearch=wordsToreplace1[i].latin; //the latin word
-                            console.log("the latin word: "+wordToSearch);
-                            wordToSearchReg = new RegExp(wordToSearch, "g");
-
-                            newWord=wordsToreplace1[i].hebrew;//the hebrew word
-                            console.log("the hebrew word: "+newWord);
-                            newPage = myHtml.replace(wordToSearchReg, "<span ><b class='press' style='background-color: greenyellow'>"+newWord+"</b></span>");
-                            document.body.innerHTML = newPage;
-                            myHtml=newPage;
-                            setClickebleFuncionToAllElements();
-                        }
-                        */
-
+                        //Passing on any of the words in order that they will be clickable.
+                        setClickebleFuncionToAllElements();
                     }
-                    else{
-                        console.log("Error on responce");
+                    else{ //error in response from the serber
+                        console.log("Error on response");
                         console.log(serverResponse);
                     }
                 }
             };
             myXMLhttpReq.send();
-            //dima
 }
 
 
 
 
+//not used for now
 function checkConnectedUser(){
     //window.onload = function () {
             chrome.storage.local.get(["talkHebrewUser"], function(items){
@@ -132,19 +179,32 @@ function checkConnectedUser(){
                     console.log("the user is: "+user);
                     console.log("no user loged to the system");
                 }
-                else{
+                else{ //user exsist
                     console.log("the user is: "+user);
                     if (document.readyState === 'complete') {
                         console.log("document ready!");
                         console.log("start processing..");
                         switchWords();
-
                     }
-                    switchWords();
                 }
-
             });
-   // }
 }
+//checkConnectedUser();
 
-checkConnectedUser();
+
+function cheackAutoReplaceSettings(){
+    console.log("back.js checking AutoReplaceSettings");
+    chrome.storage.local.get(["enableAutoReplaceWords"], function(items){
+        enableAutoReplaceWordsBool=items.enableAutoReplaceWords;
+        console.log("back.js-enableAutoReplaceWordsBool is: "+enableAutoReplaceWordsBool);
+        if(enableAutoReplaceWordsBool==true){
+            //if (document.readyState === 'complete') {
+                console.log("document ready!");
+                console.log("start processing..");
+                switchWords();
+            //}
+        }
+    });
+}
+cheackAutoReplaceSettings();
+
---
+++ client/talkHebrewExtension/popupPage.js
@@ -0,0 +1,113 @@
+/**
+ * Created by orami on 12/25/2016.
+ */
+console.log("popUp page js");
+var talkUser;
+var currenntUserSpan_element=document.getElementsByClassName("currentUser")[0];
+var enableAutoReplaceWordsBool;
+
+function cheackAutoReplaceSettings(){
+    console.log("cheacking AutoReplaceSettings");
+    var autoSwap=document.getElementById("automaticallySwap");
+    chrome.storage.local.get(["enableAutoReplaceWords"], function(items){
+        enableAutoReplaceWordsBool=items.enableAutoReplaceWords;
+        if(enableAutoReplaceWordsBool==undefined){
+            console.log("aoto isnt deined");
+            enableAutoReplaceWordsBool=true;
+            chrome.storage.local.set({ "enableAutoReplaceWords": enableAutoReplaceWordsBool }, function(){
+                console.log("enableAutoReplaceWords set to: "+enableAutoReplaceWordsBool);
+            });
+
+        }
+        else if(enableAutoReplaceWordsBool==true){
+            console.log("it is true");
+            autoSwap.style.backgroundColor="#2cbad0";
+            autoSwap.style.color="#ffffff";
+        }
+        else{
+            console.log("it is false");
+            autoSwap.style.backgroundColor="#ffffff";
+            autoSwap.style.color="#2cbad0";
+
+        }
+    });
+}
+cheackAutoReplaceSettings();
+
+ document.getElementById("changeUserReq").addEventListener("click", function() {
+     document.getElementById("popUpChangeUserForm").style.display="inline"
+     document.getElementById("changeUserReq").style.visibility = "hidden";
+ });
+
+
+ function showCureentUser(){
+     document.getElementById("popUpChangeUserForm").style.display = 'none';
+     document.getElementById("changeUserReq").style.visibility = "visible";
+
+     chrome.storage.local.get(["talkHebrewUser"], function(items){
+         console.log("test3: user is  "+items.talkHebrewUser);
+         talkUser=items.talkHebrewUser;
+         if(talkUser==undefined){
+             currenntUserSpan_element.innerHTML="לא מוגדר משתמש";
+
+         }
+         else {
+             currenntUserSpan_element.innerHTML="שלום: "+talkUser;
+         }
+     });
+ }
+showCureentUser();
+
+
+function showNumberOfWords(){
+    var numOfWords;
+    chrome.storage.local.get(["numOfAutoReplaceWordsInPage"], function(items){
+        numOfWords=items.numOfAutoReplaceWordsInPage;
+        console.log("test: magic number is  "+numOfWords);
+        document.getElementById("numberOfWordsInPage").innerHTML=numOfWords+" ";
+   });
+    var totalAmounth;
+    chrome.storage.local.get(["totalNumberOfUserWords"], function(items){
+        totalAmounth=items.totalNumberOfUserWords;
+        console.log("the total is:  "+totalAmounth);
+       document.getElementById("numberOfWordsInTotal").innerHTML=totalAmounth+" ";
+    });
+}
+showNumberOfWords();
+
+
+
+
+document.getElementById("changeUser").addEventListener("click", function() {
+    var newUser=document.getElementById("newUserName").value;
+    console.log("got new user from form: "+newUser);
+    chrome.storage.local.set({ "talkHebrewUser": newUser }, function(){
+        console.log("data saved in chrome storage:"+newUser);
+        showCureentUser();
+    });
+});
+
+document.getElementById("automaticallySwap").addEventListener("click", function(){
+    console.log("test");
+    if(enableAutoReplaceWordsBool==true){
+        chrome.storage.local.set({ "enableAutoReplaceWords": false }, function(){
+            console.log("enableAutoReplaceWords set to: "+false);
+            cheackAutoReplaceSettings()
+        });
+        //console.log("test1");
+        //enableAutoReplaceWordsBool=false;
+        //this.style.backgroundColor="#ffffff";
+        //this.style.color="#2cbad0";
+    }
+    else{
+        chrome.storage.local.set({ "enableAutoReplaceWords": true }, function(){
+            console.log("enableAutoReplaceWords set to: "+true);
+            cheackAutoReplaceSettings()
+        });
+
+        //enableAutoReplaceWordsBool=true;
+        //console.log("test2");
+        //this.style.backgroundColor="#2cbad0";
+        //this.style.color="#ffffff";
+    }
+});
